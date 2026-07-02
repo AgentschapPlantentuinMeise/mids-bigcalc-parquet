0f18c0ad80442e3f75a07bfe0fd066d7067f14b2
Mathias Dillen <37537540+matdillen@users.noreply.github.com>
2026-07-02 13:46:13 +0200

    initial commit
    Script to calculate MIDS based on a parquet file derived from a full dwc archive of GBIF preserved specimen records

---
+++ analyze_results.R
@@ -0,0 +1,88 @@
+library(arrow)
+library(bit64)
+library(dplyr)
+library(ggplot2)
+library(scales)
+
+####Analysis
+ds_test <- open_dataset("processed_output_full.parquet")
+
+# Calculate frequencies instantly
+small_int_frequencies <- ds_test |>
+  count(MIDS_level) |>
+  collect()
+
+bool_cols <- names(ds_test)[sapply(ds_test$schema$fields, function(f) f$type$ToString() == "bool")]
+
+# 2. Compute the TRUE counts for all 18 columns in a single C++ pass
+bool_summary_raw <- ds_test |>
+  summarise(
+    total_rows = n(),
+    across(all_of(bool_cols), ~sum(as.integer(.x), na.rm = TRUE))
+  ) |>
+  collect()
+
+# 3. Clean up the output into a nice, readable frequency table
+final_boolean_table <- bool_summary_raw |>
+  pivot_longer(
+    cols = -total_rows, 
+    names_to = "column_name", 
+    values_to = "true_count"
+  ) |>
+  mutate(
+    false_count = total_rows - true_count,
+    true_percentage = (true_count / total_rows) * 100
+  )
+
+print(final_boolean_table)
+
+
+### graph 
+
+#shorten mids element labels
+final_boolean_table %<>%
+  mutate(column_name = gsub("mids:MIDS","",column_name,fixed=T))
+
+# stack the table to show both the 0 and 1 counts
+true_tbl = final_boolean_table %>%
+  select(-false_count) %>%
+  mutate(achieved = "1")
+
+grap_tbl = final_boolean_table %>%
+  select(-true_count) %>%
+  mutate(achieved = "0") %>%
+  rename(true_count = false_count) %>%
+  rbind(true_tbl)
+
+# function for wrapping barchart labels
+wrap_hard_hyphen <- function(x, width = 10) {
+  vapply(x, function(s) {
+    # Split at hard width, insert hyphens
+    parts <- substring(s, seq(1, nchar(s), by = width), seq(width, nchar(s) + width - 1, by = width))
+    paste0(paste0(parts, ifelse(nchar(parts) == width, "-", "")), collapse = "\n")
+  }, character(1))
+}
+
+# cutof coordinates to draw dashlines between MIDS levels
+inf_elements = final_boolean_table %>%
+  pull(column_name)
+
+cutof1 = grep("1",inf_elements)[1] - 0.5
+cutof2 = grep("2",inf_elements)[1] - 0.5
+cutof3 = grep("3",inf_elements)[1] - 0.5
+
+# Plot the graph
+ggplot(grap_tbl, aes(x = column_name, y = true_count, fill = achieved)) +
+  labs(x = "", y = "% of Specimens achieving the MIDS element", fill = "MIDS element achieved") +
+  geom_bar(stat = "identity", position = "fill") +
+  scale_x_discrete(labels = function(x) wrap_hard_hyphen(x)) +
+  scale_y_continuous(labels = percent_format()) +
+  theme(axis.text=element_text(size=12)) +
+  scale_fill_discrete(labels = c("No",
+                                 "Yes")) +
+  geom_hline(yintercept=0.50,linetype="dotted") +
+  geom_hline(yintercept=0.75,linetype="dotted") +
+  geom_hline(yintercept=0.25,linetype="dotted") +
+  geom_vline(xintercept=cutof1,linetype="dashed",linewidth=1) +
+  geom_vline(xintercept=cutof2,linetype="dashed",linewidth=1) +
+  geom_vline(xintercept=cutof3,linetype="dashed",linewidth=1)
---
+++ parq.R
@@ -0,0 +1,282 @@
+library(arrow)
+library(bit64)
+
+# remove NULL chars as they may cause errors in the C++ backend
+options(arrow.skip_nul = TRUE)
+
+# load pointer to the parquet file
+ds <- open_dataset("../0004071-160526112335914/preserved_specimen/data_0.parquet")
+
+#dim(ds)
+#schema(ds)
+
+# path to the MIDSCalculator source code, to re-use for SSSOM parsing
+calcpath = "../../mathias/MIDSCalculator/src/"
+
+# load the same packages
+# this includes the rshiny stuff, so some overhead
+source(paste0(calcpath,"packages.R"))
+pkgLoad()
+
+# config file to use the correct sssom mappings
+config = read.ini("config.ini")
+
+# load the parsing functions from the MIDSCalculator
+source(paste0(calcpath,"parse_json_schema.R"))
+source(paste0(calcpath,"parse_data_formats.R"))
+source(paste0(calcpath,"MIDS-calc.R"))
+
+# load the local sssom mapping into a json schema compatible with the
+# midscalculator code
+# localpath = T ensures the mapping is taken from this repo, not the calculator one
+# this is needed as the relative paths in the MIDSCalculator code cause problems
+schema = parse_sssom(config = config,localpath=T)
+
+#
+## Copied code from midscalculator below, to create the lists used during the
+## calculation process
+list_UoM <- list()
+#Loop trough the values
+n_values <- length(schema$unknownOrMissing)
+for (l in 1:n_values) {
+  value = schema$unknownOrMissing[[l]]$value[[1]]
+  #only take into account values which do not count for mids
+  if (schema$unknownOrMissing[[l]]$midsAchieved == FALSE) {
+    #check if there is a property, otherwise it relates to all properties
+    if ("property" %in% names(schema$unknownOrMissing[[l]])){
+      prop <- schema$unknownOrMissing[[l]]$property[[1]]
+    } else {
+      prop <- "all"
+    }
+    #add to list
+    if (prop %in% names(list_UoM)){
+      list_UoM[[prop]] <- append(list_UoM[[prop]], value)
+    } else {
+      list_UoM[[prop]] <- value
+    }
+  }
+}
+
+midsschema <- schema[grep("mids", names(schema))]
+list_criteria <- list()
+list_props <- character()
+#Loop trough sections
+for (sect_index in seq_along(names(midsschema))){
+  #Get the contents of a section
+  section <- midsschema[[sect_index]] 
+  #Loop through conditions
+  for (cond_index in seq_along(names(section))){ 
+    crits <- ""
+    condition_name = names(section)[cond_index]
+    # Loop trough subconditions, one of these should be true (|)
+    for (subcond_index in seq_along(section[[condition_name]])){
+      # get the contents (properties etc) of a single subcondition
+      subcondition <- section[[condition_name]][[subcond_index]] 
+      #if operator is NOT, inverse all the criteria
+      if ("operator" %in% names(subcondition) && subcondition$operator == "NOT"){
+        crits <- paste0(crits, "!")}
+      #open brackets before the subcondition
+      if (subcond_index == 1){crits <- paste0(crits, "(")}
+      # Loop trough properties
+      for (prop_index in seq_along(subcondition$property)){
+        prop <- subcondition$property[[prop_index]]
+        if (is.null(prop)){break} #if there is no property, exit this loop iterating over props, still need to check later what to do when there is no property
+        #make a list of properties
+        list_props <- append(list_props, prop)
+        #add the property is not na
+        crits <- paste0(crits, "!is.na(`", prop, "`)")
+        #if there is a operator and it is not the last property, then add the matching operator to the string
+        #currently does not work if there are subconditions without property! needs to be fixed 
+        if (prop_index != length(subcondition$property) & "operator" %in% names(subcondition)){
+          if (subcondition$operator == "OR"){crits <- paste0(crits, " | ")}
+          if (subcondition$operator == "AND"){crits <- paste0(crits, " & ")}
+        }
+      }
+      #Add | between subconditions
+      if (subcond_index != length(section[[condition_name]])){crits <- paste0(crits, " | ")}
+      #close brackets after subcondition
+      else {crits <- paste0(crits, ")")}
+    }
+    #create nested list with criteria for each condition of each mids level
+    list_criteria[[names(midsschema[sect_index])]][[condition_name]] <- crits
+  }
+}
+
+list_extra_props <- c("[dwc:Occurrence]dwc:datasetKey",
+                      "[dwc:Occurrence]dwc:countryCode",
+                      "[dwc:Occurrence]dwc:kingdom",
+                      "[dwc:Occurrence]dwc:phylum", 
+                      "[dwc:Occurrence]dwc:class",
+                      "[dwc:Occurrence]dwc:order", 
+                      "[dwc:Occurrence]dwc:family",
+                      "[dwc:Occurrence]dwc:subfamily",
+                      "[dwc:Occurrence]dwc:genus")
+
+select_props = unique(c(list_props, list_extra_props, "gbifid"))
+uom = list_UoM$all
+
+## end of copied code
+
+# remove the class and namespace for the calculation with the tsv file from the sql api
+# this file has no metadata and thus no easy way to set those
+select_props_truncated = select_props %>%
+  gsub(".*:","",.)
+
+## custom function to remove the class and namespace from the filter arguments
+## that have been set up by the midscalculator code by default
+clean_backtick_texts <- function(x) {
+  if (is.list(x)) {
+    lapply(x, clean_backtick_texts)
+  } else if (is.character(x)) {
+    sapply(x, function(str) {
+      # Find all substrings enclosed in backticks
+      matches <- gregexpr("`[^`]*`", str)[[1]]
+      if (matches[1] == -1) return(str)  # No matches, return original
+      
+      # Extract those substrings
+      substrings <- regmatches(str, gregexpr("`[^`]*`", str))[[1]]
+      
+      # Process each: remove everything before final colon
+      cleaned <- sapply(substrings, function(s) {
+        content <- sub("^`(.*)`$", "\\1", s)
+        trimmed <- sub(".*:", "", content)
+        paste0("`", trimmed, "`")
+      })
+      
+      # Replace in original string
+      regmatches(str, gregexpr("`[^`]*`", str))[[1]] <- cleaned
+      str
+    }, USE.NAMES = FALSE)
+  } else {
+    x
+  }
+}
+
+list_criteria_truncated = list_criteria %>%
+  clean_backtick_texts()
+
+
+## start the reader for the parquet file
+reader <- as_record_batch_reader(ds)
+
+## define the output schema to be saved as the results parquet file
+output_schema <- schema(
+  gbifID = int64(),
+  datasetKey = string(),
+  MIDS_level = int8(),
+  `mids:MIDS0PhysicalSpecimenID` = boolean(),
+  `mids:MIDS0Organization` = boolean(),
+  `mids:MIDS1Name` = boolean(),
+  `mids:MIDS1ObjectType` = boolean(),
+  `mids:MIDS1License` = boolean(),
+  `mids:MIDS1Modified` = boolean(),
+  `mids:MIDS2QualitativeLocation` = boolean(),
+  `mids:MIDS2QuantitativeLocation` = boolean(),
+  `mids:MIDS2CollectingAgent` = boolean(),
+  `mids:MIDS2DateCollected` = boolean(),
+  `mids:MIDS2CollectingNumber` = boolean(),
+  `mids:MIDS2Media` = boolean(),
+  `mids:MIDS3GeographicalLocalityID` = boolean(),
+  `mids:MIDS3OrganizationID` = boolean(),
+  `mids:MIDS3CollectingAgentID` = boolean(),
+  `mids:MIDS3IdentifiedAsID` = boolean(),
+  `mids:MIDS3IdentifiedByID` = boolean(),
+  `mids:MIDS3MediaID`= boolean()
+)
+
+# list the colnames in the parquet file
+data_colnames = schema(ds)$names
+
+## start the output reader for saving the results
+sink <- FileOutputStream$create("processed_output_full.parquet")
+writer <- ParquetFileWriter$create(
+  schema = output_schema,
+  sink = sink,
+  properties = ParquetWriterProperties$create(
+    column_names = names(output_schema),
+    compression = "zstd" # 'zstd' offers elite compression for booleans/integers
+  )
+)
+
+#add missing columns with all values as NA
+list_props %<>% unique()
+list_props_truncated = list_props %>%
+  gsub(".*:","",.)
+missing <- c(list_props_truncated)[!c(list_props_truncated) %in% data_colnames] %>%
+  unique()
+
+# loop in batches equal to the parquet's chunk_size
+## and calculate mids for each batch
+
+ibig = 1 #progress tracker
+while (!is.null(batch <- reader$read_next_batch())) {
+  # initial timestamp
+  print(paste0("INIT batch ",ibig," at ",Sys.time()))
+  ibig = ibig + 1
+
+  #batch <- reader$read_next_batch()
+  
+  # read the data chunk in memory as a data.table object
+  chunk <- as.data.table(batch)
+  # add missing columns from the SSSOM mapping, so the calculator code doesn't crash
+  chunk[, missing] <- as.character(NA)
+  
+  ## slightly adapted MIDS calculation code below
+  
+  # change unknown or missing values for specific columns to NA
+  for (i in 1:length(list_UoM)){
+    colname <- names(list_UoM[i]) %>%
+      gsub(".*:","",.)
+    if (colname %in% names(chunk)){
+      chunk %<>%
+        mutate("{colname}" := na_if(chunk[[colname]], list_UoM[[i]]))
+    }
+  }
+  
+  # Check if separate MIDS conditions are met -------------------------------
+  
+  #For each MIDS condition in the list, check if the criteria for that condition 
+  #are TRUE or FALSE and add the results in a new column
+  for (j in 1:length(list_criteria_truncated )){
+    midslevel <- names(list_criteria_truncated [j])
+    midscrit <- list_criteria_truncated [[j]]
+    for (i in 1:length(midscrit)){
+      columnname = paste0(midslevel,  names(midscrit[i]))
+      chunk %<>%
+        mutate("{columnname}" := !!rlang::parse_expr(midscrit[[i]]))
+    }
+  }
+  
+  # Calculate MIDS level ----------------------------------------------------
+  
+  #For each MIDS level, the conditions of that level and of lower levels all need to be true
+  chunk %<>%
+    mutate(MIDS_level = case_when(
+      apply(chunk[ , grep("mids:MIDS[0-3]", names(chunk)), with = FALSE], MARGIN = 1, FUN = all) ~ 3,
+      apply(chunk[ , grep("mids:MIDS[0-2]", names(chunk)), with = FALSE], MARGIN = 1, FUN = all) ~ 2,
+      apply(chunk[ , grep("mids:MIDS[0-1]", names(chunk)), with = FALSE], MARGIN = 1, FUN = all) ~ 1,
+      apply(chunk[ , grep("mids:MIDS0", names(chunk)), with = FALSE], MARGIN = 1, FUN = all) ~ 0,
+      TRUE ~ -1
+    ))
+  
+  ## end of adapted MIDS calculation code (and end of calculation process)
+  
+  # convert the data.table to arrow table with only results columns
+  raw_table = chunk %>%
+    select(all_of(names(output_schema))) %>%
+    arrow_table()
+  
+  # convert to the correct data type in arrow
+  output_table <- raw_table$cast(output_schema)
+  
+  # write to file
+  writer$WriteTable(output_table,chunk_size = nrow(output_table))
+  
+  # end of iteration timestamp
+  print(paste0("FINISH batch ",ibig," at ",Sys.time()))
+}
+
+# close the writer pointers
+writer$Close()
+sink$close()
+
